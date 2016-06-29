3d2d3af403bbf2ba4ab22618f4416749abfae459
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-06-29 22:20:48 +0200

    WUP first barely functioning manipulation routine

--- elfeed-smarttag.el
+++ elfeed-smarttag.el
@@ -1,3 +1,5 @@
+(defvar *punctuation-re* "[,.;:()]")
+
 (defun html-dom (html-content)
   (with-temp-buffer
     (insert html-content)
@@ -17,25 +19,34 @@
       (get-text-from-html entry-content)
     (get-text-from-text entry-content)))
 
+(defun normalize-text (text)
+  "Normalize chunk of text: no punctuation symbols, all lowercase"
+  (downcase (replace-regexp-in-string *punctuation-re* "" text)))
+
+(defun text-to-bow (text)
+  "Converts a text to bag-of-words representation"
+  (split-string (normalize-text text)))
+
 (defun elfeed-smarttag-entry-bow (entry)
+  "Converts a elfeed entry into a bag-of-words"
   (let* ((entry-title (elfeed-entry-title entry))
          (entry-content-type (elfeed-entry-content-type entry))
          (entry-content-text (get-entry-text
                               (elfeed-deref (elfeed-entry-content entry))
                               entry-content-type)))
     (if (not (null entry-content-text))
-        (insert (format
-                 "%s - %s[...]\n"
-                 entry-title
-                 (substring
-                  entry-content-text 0 (min 40 (length entry-content-text))))))))
+        (text-to-bow entry-content-text))))
 
 (defun elfeed-smarttag-process-entry (entry feed)
-  (insert (elfeed-feed-id feed))
-  (elfeed-smarttag-entry-bow entry))
+  ;; (insert (elfeed-feed-id feed))
+  ;; (message (elfeed-feed-id feed))
+  (message (elfeed-entry-title entry))
+  (insert (pp (elfeed-smarttag-entry-bow entry))))
 
-(with-elfeed-db-visit (entry feed)
-  (elfeed-smarttag-process-entry entry feed)
-  ;; (elfeed-db-return)
-  )
+(with-temp-file "/tmp/elfeed-smarttag.txt" 
+  (with-elfeed-db-visit (entry feed)
+    (if (string= (elfeed-feed-id feed) "http://planet.haskell.org/rss20.xml")
+        (elfeed-smarttag-process-entry entry feed))
+    ;; (elfeed-db-return)
+    ))
 
